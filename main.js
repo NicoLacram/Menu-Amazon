91a18a715e308d1fdc4857e49ac37bf629ffcf15
NicoLacram <nico_lacrampette@hotmail.com.ar>
2019-09-26 16:05:15 -0300

    terminado

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const btnDepartamentos = document.getElementById('btn-departamentos'),
+     btnCerrarMenu = document.getElementById('btn-menu-cerrar'),
      grid = document.getElementById('grid'),
+     enlacesNav= document.querySelector('#menu .contenedor-enlaces-nav'),
+     contenedorSubcat = document.querySelector('#grid .contenedor-subcategorias'),
      esMovil = () => window.innerWidth <= 800;
 
 btnDepartamentos.addEventListener('mouseover',() =>{
@@ -9,13 +12,15 @@ btnDepartamentos.addEventListener('mouseover',() =>{
 });
 
 grid.addEventListener('mouseleave', () => {
-    grid.classList.remove('activo');
+    if(!esMovil()){
+       grid.classList.remove('activo');
+    }
 });
 
 
 document.querySelectorAll('#menu .categorias a').forEach((elemento) => {
 	elemento.addEventListener('mouseenter', (e) => {
-		if(!esDispositivoMovil()){
+		if(!esMovil()){
 			document.querySelectorAll('#menu .subcategoria').forEach((categoria) => {
 				categoria.classList.remove('activo');
 				if(categoria.dataset.categoria == e.target.dataset.categoria){
@@ -24,4 +29,62 @@ document.querySelectorAll('#menu .categorias a').forEach((elemento) => {
 			});
 		};
 	});
+});
+
+//listeners movil//
+document.querySelector('#btn-menu-barras').addEventListener('click', (e)=>{
+    e.preventDefault();
+    if(enlacesNav.classList.contains('activo')){
+        enlacesNav.classList.remove('activo');
+        document.querySelector('body').style.overflow ='visible';
+    }else{
+        enlacesNav.classList.add('activo')
+        document.querySelector('body').style.overflow ='hidden';
+    }
+});
+
+// menu departamentos movill//
+
+btnDepartamentos.addEventListener('click', (e)=>{
+    e.preventDefault();
+    grid.classList.add('activo');
+    btnCerrarMenu.classList.add('activo');
+});
+
+//btn regresar en categorias//
+
+document.querySelector('#grid .categorias .btn-regresar').addEventListener('click', (e) => {
+    e.preventDefault();
+    grid.classList.remove('activo');
+    btnCerrarMenu.classList.remove('activo');
+});
+
+document.querySelectorAll('#menu .categorias a').forEach((elemento) => {
+    elemento.addEventListener('click', (e) =>{
+        if(esMovil()){
+            contenedorSubcat.classList.add('activo');
+            document.querySelectorAll('#menu .subcategoria').forEach((categoria)=>{
+                categoria.classList.remove('activo');
+                if(categoria.dataset.categoria == e.target.dataset.categoria){
+                    categoria.classList.add('activo');
+                }
+            })
+        }
+    });
+});
+
+//btn regresar en subcategorias//
+document.querySelectorAll('#grid .contenedor-subcategorias .btn-regresar').forEach((boton)=> {
+    boton.addEventListener('click', (e) => {
+        e.preventDefault();
+        contenedorSubcat.classList.remove('activo');
+    });
+});
+
+btnCerrarMenu.addEventListener('click', (e)=>{
+    e.preventDefault();
+    document.querySelectorAll('#menu .activo').forEach((elemento) =>{
+        elemento.classList.remove('activo');
+    });
+    document.querySelector('body').style.overflow = 'visible';
 });
